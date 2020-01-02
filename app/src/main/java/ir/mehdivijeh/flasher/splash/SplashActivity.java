66a0f4ba81f95cceed6ca7b5bf82437257ed751f
mehdi <mehdivijeh@gmail.com>
2020-01-03 03:07:33 +0330

    [+] adds shimmer

--- app/src/main/java/ir/mehdivijeh/flasher/splash/SplashActivity.java
+++ app/src/main/java/ir/mehdivijeh/flasher/splash/SplashActivity.java
@@ -1,19 +1,18 @@
 package ir.mehdivijeh.flasher.splash;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import ir.mehdivijeh.flasher.R;
 import ir.mehdivijeh.flasher.general.repo.db.LocalDb;
 import ir.mehdivijeh.flasher.main.repo.LocalCollectionRepo;
 import ir.mehdivijeh.flasher.main.repo.db.CollectionDao;
 import ir.mehdivijeh.flasher.splash.presenter.SplashPresenterImpl;
-import ir.mehdivijeh.flasher.study.StudyActivity;
 import ir.mehdivijeh.flasher.word.WordActivity;
 
-public class SplashActivity extends AppCompatActivity implements SplashContract.SplashView{
+public class SplashActivity extends AppCompatActivity implements SplashContract.SplashView {
 
     private SplashContract.SplashPresenter mPresenter;
 
@@ -25,17 +24,17 @@ public class SplashActivity extends AppCompatActivity implements SplashContract.
         initPresenter();
         mPresenter.checkIsFirstTimeOpenApp();
 
-        startActivity(new Intent(this , WordActivity.class));
+        startActivity(new Intent(this, WordActivity.class));
     }
 
-    private void initView(){
+    private void initView() {
 
     }
 
-    private void initPresenter(){
+    private void initPresenter() {
         CollectionDao collectionDao = LocalDb.getInstance(this).collectionDao();
         LocalCollectionRepo localCollectionRepo = new LocalCollectionRepo(collectionDao);
-        mPresenter = new SplashPresenterImpl(this,localCollectionRepo);
+        mPresenter = new SplashPresenterImpl(this, localCollectionRepo);
     }
 
     @Override
--- app/src/main/java/ir/mehdivijeh/flasher/word/WordActivity.java
+++ app/src/main/java/ir/mehdivijeh/flasher/word/WordActivity.java
@@ -1,8 +1,12 @@
 package ir.mehdivijeh.flasher.word;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Group;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import ir.mehdivijeh.flasher.R;
 
@@ -12,5 +16,12 @@ public class WordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word);
+
+        Group group = findViewById(R.id.group_no_word);
+        group.setVisibility(View.GONE);
+
+        ShimmerFrameLayout container =
+                findViewById(R.id.shimmer_view_container);
+        container.startShimmer();
     }
 }
